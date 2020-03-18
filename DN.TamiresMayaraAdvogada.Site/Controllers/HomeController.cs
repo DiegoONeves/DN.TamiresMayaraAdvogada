72fe185f1a612dbbd1a9d30783914aa0948bbca0
Diego Neves <diegoneves89@hotmail.com>
2020-03-18 15:36:25 -0300

    primeiro commit

---
+++ DN.TamiresMayaraAdvogada.Site/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using DN.TamiresMayaraAdvogada.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace DN.TamiresMayaraAdvogada.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
